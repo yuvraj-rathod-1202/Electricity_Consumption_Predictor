45b9eafa9ec1b6ae77dcb37f80c201be4c4ca5dd
yuvraj-rathod-1202 <rathodyuvraj12022007@gmail.com>
2025-05-25 17:04:23 +0530

    EDA

--- extract_ingest/ingest.py
+++ extract_ingest/ingest.py
@@ -7,7 +7,7 @@ class DataIngestor(ABC):
     def ingest(self, filePath: str):
         pass
 
-class DataFrameIngest(DataIngestor):
+class IngestDataFromCSV(DataIngestor):
     def ingest(self, filePath):
         extension = os.path.splitext(filePath)[1].lower()
         if extension != ".csv":
@@ -15,10 +15,23 @@ class DataFrameIngest(DataIngestor):
         
         df = pd.read_csv(filePath)
         return df
-
-class Ingestor:
+    
+class IngestDataFromText(DataIngestor):
     def ingest(self, filePath):
-        return DataFrameIngest().ingest(filePath)
+        extension = os.path.splitext(filePath)[1].lower()
+        if extension != ".txt":
+            raise ValueError("File is not a valid txt type")
+        
+        df = pd.read_csv(filePath, delimiter=';')
+        return df
+
+class Ingestor():
+    def ingest(self, filePath):
+        extension = os.path.splitext(filePath)[1].lower()
+        if extension == ".csv":
+            return IngestDataFromCSV().ingest(filePath)
+        elif extension == ".txt":
+            return IngestDataFromText().ingest(filePath)
 
 if __name__ == "__main__":
     # ingestor = Ingestor()
